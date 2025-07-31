41d10b2bd40f8c3f4b855a048fa065941a3f2da5
Atik yumbay <amaruyum@gmail.com>
2025-07-31 16:47:02 -0500

    Proyecto Reto Tecnico Java

---
+++ src/main/java/com/github/amaruyum/clientes_personas/ClientesPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.github.amaruyum.clientes_personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesPersonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/amaruyum/clientes_personas/ClientesPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.amaruyum.clientes_personas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
